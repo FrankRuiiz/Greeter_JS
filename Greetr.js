a5b1a345942a828134e3ef57c484496b40adf629
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-21 22:51:54 -0700

    added more methods to the proto - Frank

--- Greetr.js
+++ Greetr.js
@@ -4,14 +4,79 @@
         return new Greetr.init(firstName, lastName, language);
     };
 
-    Greetr.prototype = {}; // This is where all Greetr methods will live
+    var supportedLangs = ['en', 'es'];
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        em: 'Logged in',
+        es: 'Inicio Sesion'
+    };
+
+    Greetr.prototype = {  // This is where all Greetr methods will live
+
+        fullName: function() {
+            return this.fullName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+
+            var msg;
+
+            //if undefined or null it will be coerced to false
+            if(formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+            if(console) {
+                console.log(msg);
+            }
+            // this refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+        log: function() {
+            if(console) {
+                console.log( logMessages[this.language] + ': ' + this.fullName() )
+            }
+
+            return this;
+        }
+
+
+    };
 
     Greetr.init = function(firstName, lastName, language) {
 
         var self =  this;
-        self.firstName = firstName || "";
-        self.lastName = lastName || "";
-        self.language = language || "en";
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
 
     };
 
